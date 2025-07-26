4ed608329bb09f97149ca6dafb43cb64a623f7de
gkouvi <gkouvi@gmail.com>
2025-07-26 21:29:27 +0300

    initialization_DPP_26072025_2128

---
+++ src/main/java/gr/uoi/dit/master2025/gkouvas/dpp/DppApplication.java
@@ -0,0 +1,14 @@
+package gr.uoi.dit.master2025.gkouvas.dpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/uoi/dit/master2025/gkouvas/dpp/DppApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uoi.dit.master2025.gkouvas.dpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
